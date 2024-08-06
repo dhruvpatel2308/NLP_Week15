5eac254b1ab8f6fdbf1130123c69d7412b000fea
dhruvpatel2308 <56676428+dhruvpatel2308@users.noreply.github.com>
2024-08-06 14:46:34 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import requests
+
+#streamlit
+st.title("GPT-2 Chatbot")
+
+st.write("This is a chatbot powered by a fine-tuned GPT-2 model. Ask any question and get a response!")
+
+user_input = st.text_input("You: ","")
+
+if st.button("Send"):
+  if user_input:
+    #send the user input to the FastAPI backend
+    response = requests.post('https://',json={'prompt':user_input})
+    response_json = response.json()
+    bot_response = response_json['response']
+    st.text_area("Bot:", bot_response, height = 200)
+
+  else:
+    st.write("Please enter a question to get a response.")
+
+if __name__ == '__main__':
+  st.run()
